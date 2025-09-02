78ddad19cad7de3612a68a47d231af3d8683a0de
G-Ludovic <l.galicher@free.fr>
2025-09-02 11:41:52 +0200

    setting up the component carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,23 @@
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./Carousel.css";
 
-export const Carousel = () => {
-  return <div>Carousel</div>;
+export const Carousel = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div className="carousel">
+      <BsArrowLeftCircleFill />
+      {data.map((item, index) => {
+        return (    
+            <img src={item.src} alt={item.alt} key={index.item}/>    
+        );
+      })}
+      <BsArrowRightCircleFill />
+      <span>
+        {data.map((_, index) => {
+          return <button type="button" key={index.item} onClick={null}></button>
+        })}
+      </span>
+    </div>
+  ); 
 };
